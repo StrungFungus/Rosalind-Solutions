08e5408ff2706086ea841d0e1ca6bcdc0335f64b
StrungFungus <163358618+StrungFungus@users.noreply.github.com>
2025-05-22 12:24:13 -0400

    Add files via upload

---
+++ Bioinformatics Stronghold/HAMM.py
@@ -0,0 +1,18 @@
+import unittest
+
+def point_mutation_counter(seq1, seq2):
+    differences = sum(1 for str1, str2 in zip(seq1, seq2) if str1 != str2)
+    return differences
+
+
+a = input("Input first strand:")
+b = input("Input second strand:")
+print(point_mutation_counter(a, b))
+
+# class TestRabbitsFunction(unittest.TestCase):
+#
+#     def test_sample_case(self):
+#         self.assertEqual(point_mutation_counter("GAGCCTACTAACGGGAT", "CATCGTAATGACGGCCT"), 7)
+#
+# if __name__ == '__main__':
+#     unittest.main()
