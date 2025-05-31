3d4c478464096c6f409f9ace6d3f770c5d08572a
StrungFungus <163358618+StrungFungus@users.noreply.github.com>
2025-05-31 19:13:32 -0400

    Add files via upload

---
+++ Bioinformatics Stronghold/PROT.py
@@ -0,0 +1,42 @@
+RNA_Codons = {
+    # 'M' - START, '_' - STOP
+    "GCU": "A", "GCC": "A", "GCA": "A", "GCG": "A",
+    "UGU": "C", "UGC": "C",
+    "GAU": "D", "GAC": "D",
+    "GAA": "E", "GAG": "E",
+    "UUU": "F", "UUC": "F",
+    "GGU": "G", "GGC": "G", "GGA": "G", "GGG": "G",
+    "CAU": "H", "CAC": "H",
+    "AUA": "I", "AUU": "I", "AUC": "I",
+    "AAA": "K", "AAG": "K",
+    "UUA": "L", "UUG": "L", "CUU": "L", "CUC": "L", "CUA": "L", "CUG": "L",
+    "AUG": "M",
+    "AAU": "N", "AAC": "N",
+    "CCU": "P", "CCC": "P", "CCA": "P", "CCG": "P",
+    "CAA": "Q", "CAG": "Q",
+    "CGU": "R", "CGC": "R", "CGA": "R", "CGG": "R", "AGA": "R", "AGG": "R",
+    "UCU": "S", "UCC": "S", "UCA": "S", "UCG": "S", "AGU": "S", "AGC": "S",
+    "ACU": "T", "ACC": "T", "ACA": "T", "ACG": "T",
+    "GUU": "V", "GUC": "V", "GUA": "V", "GUG": "V",
+    "UGG": "W",
+    "UAU": "Y", "UAC": "Y",
+    "UAA": "_", "UAG": "_", "UGA": "_"
+}
+
+seq = input("Input RNA Sequence:")
+
+
+def RNA_to_protein(seq):
+    protein = ""
+    for i in range(0, len(seq) - 2, 3):
+        codon = seq[i:i + 3]
+        amino_acid = RNA_Codons.get(codon)
+        if amino_acid == "_":
+            break
+        protein += amino_acid
+    return protein
+
+
+print("Protein:", RNA_to_protein(seq))
+
+
